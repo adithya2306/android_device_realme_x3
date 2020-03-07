193802718b9ff6dd43de451489e97a7c5aadb927
dev-harsh1998 <dev-harsh1998@hotmail.com>
2020-03-07 21:17:53 +0530

    x3: Import and build custom light HAL
    * based on oneplus sm8150 and rc has been stripped down of graphic group
    * scale brightness from 255 to max (4095)
    * override and bind mount oem's light hal to /dev/null
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.x3
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
