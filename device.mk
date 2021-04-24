9f3b887cebbf91907a8103ff669b5d285f269e2b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-24 15:16:13 +0530

    x3: Build RealmeDoze and show in settings

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Doze
+PRODUCT_PACKAGES += \
+    RealmeDoze
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
