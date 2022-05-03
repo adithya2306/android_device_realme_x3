96c169f3d3de81f02475cefb4f16a1aa8c27bc22
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-03 18:10:54 +0530

    x3: Import qcom recovery init script
    https://git.codelinaro.org/clo/la/device/qcom/common/-/raw/LA.UM.9.1.r1-11800.05-SMxxx0.QSSI12.0/rootdir/etc/init.recovery.qcom.rc
    
     * Remove brightness setting

--- device.mk
+++ device.mk
@@ -70,9 +70,10 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/bin/init.set_baseband.sh:$(TARGET_COPY_OUT_PRODUCT)/bin/init.set_baseband.sh \
     $(LOCAL_PATH)/rootdir/etc/init.device.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.device.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.safailnet.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.safailnet.rc
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.safailnet.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.safailnet.rc \
+    $(LOCAL_PATH)/rootdir/bin/init.set_baseband.sh:$(TARGET_COPY_OUT_PRODUCT)/bin/init.set_baseband.sh
 
 # Kernel
 PRODUCT_COPY_FILES += \
